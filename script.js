31e6384e34aa73bc295cfb39d7c38053b5b914b0
mehabubi-meh <mehabubimeh0708@gmail.com>
2025-07-02 07:49:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", function () {
+  const statusPara = document.getElementById("status");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = "Entered Metaverse";
+
+  statusPara.replaceWith(h1);
+});
